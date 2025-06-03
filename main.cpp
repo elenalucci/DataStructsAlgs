5ef5fec59c760ac01d6a2186bb7e9c7dcf61d3da
elenalucci <enl14@students.uwf.edu>
2025-06-02 22:36:55 -0500

    made some plans

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@ int main(){
    cout << "Select a Data Structure or Algorithm: \n";
    
    int selection;
-   
+   //doubly linked list, stack, queue. BST, Hash Table, Graph BFS/DFS, Heap (Min/Max), Sorting Alg
    do{
-      cout << "   1.) Linked List \n";
-      cout << "   2.) Quit\n";
+      cout << "   1.) Singly Linked List \n"; //implemented
+      cout << "   2.) Doubly Linked List \n";
+      cout << "   3.) Stack \n";
+      cout << "   4.) Queue \n";
+      cout << "   5.) Binary Search Tree \n";
+      cout << "   6.) Hash Table \n";
+      cout << "   7.) Graph Breadth First Seach \n";
+      cout << "   8.) Graph Depth First Search \n";
+      cout << "   9.) Heap \n";
+      cout << "   10.) Sorting Algorithm \n"
+      cout << "   11.) Quit \n";
       cin>> selection;
       
       if(selection == 1){
@@ -18,7 +27,7 @@ int main(){
          lnkdlst.run();
       }
       
-   }while(selection != 2);
+   }while(selection != 11);
    
    
    return 0;
